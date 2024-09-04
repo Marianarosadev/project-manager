e172f3a49febc28759f08284d1bceb8633548c6f
marianarosa <mary.oliveira0212@gmail.com>
2024-09-04 16:36:12 -0300

    Instalação e configuração do Vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,33 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   srcDir: 'src/',
   css: ['~/assets/styles/main.scss'],
-  modules: ['@pinia/nuxt'],
+  modules: [
+    '@pinia/nuxt',
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
+  build: {
+    transpile: ['vuetify'],
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+    },
+  },
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
     },
   },
 })
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,4 +2,5 @@
   <h1 class="text-3xl font-bold underline">
     Página principal
   </h1>
+  <v-btn>teste</v-btn>
 </template>
