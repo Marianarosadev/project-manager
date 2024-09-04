c7a67dcdceac5b97928524d590eb1ab72bc4de93
marianarosa <mary.oliveira0212@gmail.com>
2024-09-04 18:46:32 -0300

    Configuração do layout default, app.vue e criação do componente navbar

---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import Logo from '~/assets/images/logo.svg'
+</script>
+
+<template>
+  <nav class="bg-secondary p-4 flex items-center justify-center">
+    <img :src="Logo" alt="Logo gerenciador de projetos">
+  </nav>
+</template>
+
